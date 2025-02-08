68acd97bb85dc6326d587c7bee2f393bde2d9b60
IftekarRahmanRuhit <iftekarrahmanruhit@gmail.com>
2025-02-08 23:45:30 +0600

    updated

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -22,23 +22,3 @@ export default async function Blog ({params}) {
 };
 
 
-
-// import { getBlog } from '@/lib/getBlog';
-
-// export default async function BlogPost({params}) {
-//      const {id} = params
-
-//     const blog = await getBlog(id)
-
-//   return (
-//     <div className="container mx-auto my-12 px-4 sm:px-6 lg:px-8">
-//       <div className="bg-white shadow-lg rounded-lg overflow-hidden">
-//         <div className="p-6">
-//           <h1 className="text-3xl font-bold mb-4">{blog.title}</h1>
-//           <p className="text-gray-700 leading-relaxed">{blog.body}</p>
-//           <p className="text-gray-500 text-sm mt-4">Post ID: {blog.id}</p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
